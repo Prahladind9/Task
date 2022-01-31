aaab45c218fd22666d5ef6db1ce468f86e8e324d
Prahlad Rao <prahladskingdom@hotmail.com>
2022-01-31 14:18:44 +0530

    FC: Initial Commit

---
+++ friday/src/test/java/edu/prao/friday/FridayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prao.friday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
